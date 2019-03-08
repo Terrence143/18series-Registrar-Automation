8bf52fa308360d376a5c03239eae8247051367d2
Glen Escuadra <glen.escuadra@g.msuiit.edu.ph>
2019-03-09 01:11:27 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit e003c6e3951781866ad80b319ca937e2757058e3.

--- boop/routes.py
+++
@@ -1,8 +0,0 @@
-from boop import app, db
-from flask import render_template, url_for, flash, redirect, request, abort, jsonify, session
-from boop.forms import *
-from boop.models import *
-
-@app.route("/", methods=["GET", "POST"])
-def welcome():
-    
